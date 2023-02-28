8b8f4f48923aee148ba66cdabf35a612a8844cf9
sslukess <lvss.dev@gmail.com>
2023-02-28 00:35:31 +0000

    more initial set up

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = ({children,}: {children: React.ReactNode;}) => {
+
+    return (
+    <section><div>I AM A HEADER</div>{children}</section>);
+
+}
+  
+
+export default MainLayout;
