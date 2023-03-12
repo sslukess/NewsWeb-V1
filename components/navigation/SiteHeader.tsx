43672eadcd0bb721b27c43c90da28ffe247b352e
sslukess <lvss.dev@gmail.com>
2023-03-12 06:50:22 +0000

    add search bar

--- components/navigation/SiteHeader.tsx
+++ components/navigation/SiteHeader.tsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Container, Form, InputGroup } from 'react-bootstrap';
 import styled from 'styled-components';
 
 // placeholder for tags/navs 
@@ -21,19 +21,19 @@ const HeaderContainer = styled(Container)`
     color: ${({ theme }) => theme.text.colors.primary};
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    align-items: end;
     padding: 0;
     border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
 `;
 
 // Header element 
 const HeaderElement = styled.div`
-    padding: 1rem;
+    padding: 10px;
 `;
 
 // Logo wrapper 
 const LogoWrapper = styled.div`
-    padding: 1rem;
+    // padding: 1rem;
     width: 20em;
 `;
 
@@ -55,7 +55,12 @@ function SiteHeaderBar(props) {
                 </LogoWrapper>
             </HeaderElement>
             <HeaderElement>
-                <h1>Nav</h1>
+                <Form>
+                    <InputGroup>
+                    {/* <InputGroup.Text>search</InputGroup.Text> */}
+                    <Form.Control type="text" placeholder='search'/>
+                    </InputGroup>
+                </Form>
             </HeaderElement>
         </HeaderContainer>
 
