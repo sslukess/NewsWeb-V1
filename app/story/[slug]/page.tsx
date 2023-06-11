eedaf9045b2a934625aec23a59287088be0ddeae
sslukess <lvss.dev@gmail.com>
2023-06-11 02:49:44 +0000

    migrate story routes to App router

--- app/story/[slug]/page.tsx
+++ app/story/[slug]/page.tsx
@@ -1,24 +1,28 @@
-import { GetStaticProps, GetStaticPaths } from 'next'
-import apolloClient from '../../graphql/Client'
-import { ALL_STORIES_QUERY } from '../../graphql/queries/AllStories'
+import apolloClient from '../../../graphql/Client'
+import { ALL_STORIES_QUERY } from '../../../graphql/queries/AllStories'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
+// disable all routes not returned from generateStaticParams
+export const dynamicParams = false;
+
 // getStaticPaths
-export const getStaticPaths: GetStaticPaths = async () => {
+export const generateStaticParams = async () => {
 
     const { data } = await apolloClient.query({
         query: ALL_STORIES_QUERY,
     })
 
-    const paths = data.storyCollection.items.map((item) => ({
-        params: { slug: item.slug },
-    }))
+    const paths = data.storyCollection.items.map((item) => { 
+        return { slug: item.slug }
+    });
 
-    return { paths, fallback: false }
+    console.log(paths);
+
+    return paths;
 }
 
 // getStaticProps
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+const getStoryContents = async ( params ) => {
     const { data } = await apolloClient.query({
         query: ALL_STORIES_QUERY,
     });
@@ -28,18 +32,21 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         (item) => item.slug === params?.slug
     );
 
-    return {
-        props: {
-            story: storyData,
-        },
-    }
+    return storyData;
 }
 
 // StoryPage component
-const StoryPage = ({ story }) => {
+const StoryPage = async ({ params }) => {
+
+    console.log(params);
+
+    // get the story data
+    const story = await getStoryContents( params );
 
     // get the data for the story page
     const {storyTitle, storyText, storyPhoto } = story;
+
+    // if there is missing data, 
 
     // Convert the storyText Rich Text json to JSX
     const StoryTextJSX = documentToReactComponents(storyText.json);
