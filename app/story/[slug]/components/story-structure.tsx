43a15480e1042afb4e6cc3f74b803d34ea45ecc3
sslukess <lvss.dev@gmail.com>
2023-07-28 05:27:14 +0000

    update story page

--- app/story/[slug]/components/story-structure.tsx
+++ app/story/[slug]/components/story-structure.tsx
@@ -2,16 +2,43 @@
 
 import styled from 'styled-components'
 import { Col, Row, Container } from "react-bootstrap";
+import Image from 'next/image';
+
+import { breakpoint, breakpointSizes } from '../../../../styling/style-mix-ins/CssBreakpoints';
 
 // Styles 
 const StoryContainer = styled(Container)`
-    padding: 3rem;    
+
     display: flex;
+
+    padding: 1rem;
+    ${breakpoint.up(breakpointSizes.lg)`
+    padding: 3rem 6rem;
+    `}
 `
 
-const StoryImage = styled.img`
-    margin: 5px;
+const StoryImage = styled(Image)`
+    padding: 5px;
     width: 100%;
+    object-fit: cover;
+`
+const StoryImageWrapper = styled.div`
+    position: relative !important;
+    
+    padding: 10px;
+
+    // height for mobile and desktop 
+
+    ${breakpoint.up(breakpointSizes.lg)`
+    height: 500px; 
+    width: 90%;
+    `}
+
+    ${breakpoint.down(breakpointSizes.lg)`
+    height: 300px;
+    width: 100%;
+    `}
+   
 `
 
 const StoryHeading = styled.h1`
@@ -26,6 +53,10 @@ const StoryText = styled.div`
 
 `
 
+const HeadingWrapper = styled.div`
+    display: flex;
+`;
+
 function StoryPageStructure(props) {
 
     const { storyTitle, storyBody, storyPhotoURL, author, storyDate } = props;
@@ -33,14 +64,24 @@ function StoryPageStructure(props) {
     return (
         <StoryContainer fluid>
             <Row>
-                <Col md={8} sm>
+                <Col lg={8}>
+
                     <StoryHeading>{storyTitle}</StoryHeading>
-                    <StoryByLine>{storyDate} - {author}</StoryByLine>
+
+                    <StoryImageWrapper>
+                        <StoryImage src={storyPhotoURL}
+                            layout={'fill'}
+                            alt={'XXX'} />
+                    </StoryImageWrapper>
+
+                    <StoryByLine>{storyDate} {author ? " - " + author : null}</StoryByLine>
+
                     <StoryText>{storyBody}</StoryText>
                 </Col>
 
-                <Col xs={{order: 'first'}} sm={{order: 'last'}}>
-                    <StoryImage src={storyPhotoURL} />
+                <Col>
+                    <></>
+                    {/* Unused Col currently */}
                 </Col>
             </Row>
         </StoryContainer>
