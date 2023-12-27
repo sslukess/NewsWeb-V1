a7e6f5c40d8dcc89e3613f378fbe331d0d115ceb
sslukess <lvss.dev@gmail.com>
2023-12-27 03:57:50 +0000

    search box is styled

--- src/components/component-building-blocks/molecules/SearchBar.tsx
+++ src/components/component-building-blocks/molecules/SearchBar.tsx
@@ -1,13 +1,24 @@
+'use client';
+
+import styled from 'styled-components';
+
+import theme from '@/styling/CSS/theme/theme'
+
 // sending the form submission to this page with the included query parameters.
 // The page itself will interpret the term, data fetch and then display results. 
 const searchResultsPage: string = "/topic/search-results"
 
+const StyledSearchBox = styled.input`
+    padding: 4px;
+    border: 2px solid ${theme.colors.grey};
+    border-radius: 11px;
+`
 
 const SearchBar = () => {
 
     return (
         <form action={searchResultsPage}>
-                <input type="text" placeholder='search' name="searchTerm" id="searchTerm" />
+                <StyledSearchBox type="text" placeholder='search...' name="searchTerm" id="searchTerm" />
         </form>
     )
 }
